8b304ee945cc39a7107b9ce7434f474cc4153a80
Slawomir Borowiec <przodownik@tlen.pl>
2016-04-22 11:29:59 +0200

    simple criteria select added

--- src/test/java/pl/java/scalatech/simple/jpa/criteria/SelectTest.java
+++ src/test/java/pl/java/scalatech/simple/jpa/criteria/SelectTest.java
@@ -6,8 +6,10 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
 import javax.persistence.criteria.Root;
 
 import org.assertj.core.api.Assertions;
@@ -56,8 +58,55 @@ public class SelectTest extends JPAUnitTestCase {
             criteria.select(item);
             criteria.orderBy(builder.desc(item.get("name")));
             List<Item> items = em.createQuery(criteria).getResultList();
-            log.info("result : {}",items);
+            log.info("result : {}", items);
             Assertions.assertThat(items.size()).isEqualTo(10);
+        } catch (Exception ex) {
+            log.info("{}", ex);
+        } finally {
+            em.close();
+        }
+    }
+    @Test
+    public void should_SELECT_B_DATA(){
+        EntityManager em = EntityManagerUtils.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Item> q = cb.createQuery(Item.class);
+            Root<Item> c = q.from(Item.class);
+            q.select(c);
+            TypedQuery<Item> query = em.createQuery(q);
+            List<Item> results = query.getResultList();
+            log.info("result : {}", results);
+            Assertions.assertThat(results.size()).isEqualTo(10);
+        } catch (Exception ex) {
+            log.info("{}", ex);
+        } finally {
+            em.close();
+        }
+    }
+   //TypedQuery<Item> query =           em.createQuery("FROM Item", Item.class);
+   // List<Item> results = query.getResultList();
+
+    @Test
+    public void should_SELECT_C_PARAMETERS_DATA(){
+        EntityManager em = EntityManagerUtils.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Item> q = cb.createQuery(Item.class);
+            Root<Item> c = q.from(Item.class);
+            q.select(c);
+
+
+            ParameterExpression<BigDecimal> p = cb.parameter(BigDecimal.class);
+            q.select(c).where(cb.gt(c.get("price"), p));
+            TypedQuery<Item> query = em.createQuery(q);
+            query.setParameter(p, BigDecimal.valueOf(103));
+            List<Item> results = query.getResultList();
+
+            log.info("result : {}", results);
+            Assertions.assertThat(results.size()).isEqualTo(6);
         } catch (Exception ex) {
             log.info("{}", ex);
         } finally {
